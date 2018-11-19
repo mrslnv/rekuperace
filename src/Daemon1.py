f2be26e5fe022abaf12002143f2abb976c6c8a64
mrslnv <mrslnv@nowhere.com>
2018-11-19 06:20:21 +0100

    better time format

---
+++ src/Daemon1.py
@@ -0,0 +1,67 @@
+from client import *
+from time import sleep
+import time
+from time import strftime
+
+class EngineState:
+    lastChange = time.time()
+    currentPower = 0
+    lastStopTime = 0
+    lastHourUsage = 0
+
+    def changePower(self,newPower):
+        if self.currentPower == newPower:
+            return
+        if newPower == 0:
+            self.lastStopTime = time.time()
+
+        self.currentPower = newPower
+
+
+# SLEEP_TIME = 10
+SLEEP_TIME = 10
+ENGINE_POWER = 20
+RESTART_DELAY = 600
+
+def now():
+    return strftime('%H:%M:%S %d.%m.',time.localtime(time.time()))
+
+while True:
+    try:
+        cl = client.VentboxClient()
+        # cl = client.TestClient()
+
+        es = EngineState()
+        while True:
+            t = cl.getTempExtIn()
+            print(now()," Temp:",t)
+
+            desiredPower = ENGINE_POWER
+            if t<=6:
+                print(now()," Motor - stop")
+                desiredPower = 0
+            else:
+                if es.currentPower > 0:
+                    print(now()," Motor - already running ")
+                    desiredPower = ENGINE_POWER
+                else:
+                    if (time.time() - es.lastStopTime) > RESTART_DELAY:
+                        print(now()," Motor - starting ")
+                        desiredPower = ENGINE_POWER
+                    else:
+                        print(now()," Motor - not starting, delta:", (time.time() - es.lastStopTime))
+                        desiredPower = 0
+
+            print(now()," Setting power:",desiredPower)
+            cl.setPower(desiredPower)
+            es.changePower(desiredPower)
+            sleep(SLEEP_TIME)
+    except Exception as e:
+        print(now()," Error:",e)
+        try:
+            cl.closeReku()
+        except Exception as e2:
+            print(now()," Error while closing:",e)
+
+    print(now()," Retry")
+    sleep(20)
--- src/client/client.py
+++ src/client/client.py
@@ -3,6 +3,7 @@ import logging
 import struct
 import numpy as np
 
+
 class AbstractClient:
     def __init__(self):
         pass
@@ -10,8 +11,12 @@ class AbstractClient:
     def getTempExtIn(self) -> float:
         return 0
 
-    def setPower(self,power:int) -> None:
+    def setPower(self, power: int) -> None:
         pass
+
+    def closeReku(self) -> None:
+        pass
+
 
 class VentboxClient(AbstractClient):
     def __init__(self) -> None:
@@ -22,15 +27,18 @@ class VentboxClient(AbstractClient):
 
     def getTempExtIn(self) -> float:
         reg = self.client.read_input_registers(0, 2)
-        s = struct.pack('<HH',reg.registers[0],reg.registers[1])
-        return struct.unpack('f',s)[0]
+        s = struct.pack('<HH', reg.registers[0], reg.registers[1])
+        return struct.unpack('f', s)[0]
 
-    def setPower(self,power:int) -> None:
+    def setPower(self, power: int) -> None:
         self.power = power
         s = struct.pack('>f', power)
         i1, i2 = struct.unpack('>HH', s)
         self.client.write_register(6, i2)
         self.client.write_register(6 + 1, i1)
+
+    def closeReku(self):
+        self.client.close()
 
 
 class TestClient(AbstractClient):
@@ -38,19 +46,25 @@ class TestClient(AbstractClient):
         super().__init__()
         self.i = -1
         self.data = np.empty(500)
-        for x in range(100):
-            self.data[5*x] = 5.1
-            self.data[5*x+1] = 5.2
-            self.data[5*x+2] = 4.8
-            self.data[5*x+3] = 5.1
-            self.data[5*x+4] = 4.8
+        # for x in range(5):
+        #     self.data[5*x] = 5.1
+        #     self.data[5*x+1] = 5.2
+        #     self.data[5*x+2] = 4.8
+        #     self.data[5*x+3] = 5.1
+        #     self.data[5*x+4] = 4.8
+
+
+        for x in range(1, 5):
+            self.data[5 * x] = 5.1
+            self.data[5 * x + 1] = 5.2
+            self.data[5 * x + 2] = 4.8
+            self.data[5 * x + 3] = 5.1
+            self.data[5 * x + 4] = 4.8
 
     def getTempExtIn(self) -> float:
         self.i += 1
         t = self.data[self.i]
-        print("t=",t)
         return t
 
-    def setPower(self,power:int):
+    def setPower(self, power: int):
         pass
-
